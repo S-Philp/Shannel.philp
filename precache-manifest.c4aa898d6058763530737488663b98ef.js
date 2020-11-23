054c604aa7bdf458301f7cfd3d5def5352cc199a
S-Philp <shannel.philp@yahoo.com>
2020-11-23 11:39:32 -0600

    Updates

---
+++ precache-manifest.c4aa898d6058763530737488663b98ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b94b22daa9e53093254f6b47f13a629",
+    "url": "/S-Philp-Portfolio.github.io/index.html"
+  },
+  {
+    "revision": "4c8a0e67ace41653d93b",
+    "url": "/S-Philp-Portfolio.github.io/static/css/main.f218c33a.chunk.css"
+  },
+  {
+    "revision": "c235cf04d273f2b5d2b0",
+    "url": "/S-Philp-Portfolio.github.io/static/js/2.fc908433.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/S-Philp-Portfolio.github.io/static/js/2.fc908433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c8a0e67ace41653d93b",
+    "url": "/S-Philp-Portfolio.github.io/static/js/main.af07ec7e.chunk.js"
+  },
+  {
+    "revision": "0bb8bbfa4f58a08ec1a5",
+    "url": "/S-Philp-Portfolio.github.io/static/js/runtime-main.7bebfbf1.js"
+  },
+  {
+    "revision": "b0958fbf53ecfbd72687b9494d3eb3dd",
+    "url": "/S-Philp-Portfolio.github.io/static/media/Me_copy.b0958fbf.jpg"
+  },
+  {
+    "revision": "2c7147ffe3849075826e1b08d30abecb",
+    "url": "/S-Philp-Portfolio.github.io/static/media/resume.2c7147ff.pdf"
+  }
+]);
